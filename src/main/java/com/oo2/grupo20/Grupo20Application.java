98101449b1823df9fba05990d64860c6ebb1a40a
matbas99 <nodnerb99m@gmail.com>
2025-06-04 16:08:37 -0300

    Commit de testeo.

--- src/main/java/com/oo2/grupo20/Grupo20Application.java
+++ src/main/java/com/oo2/grupo20/Grupo20Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Grupo20Application {
-
+	
+	//Commit de testeo
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo20Application.class, args);
 	}
