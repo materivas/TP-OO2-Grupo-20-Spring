bd884ea18a2519f22135b371a959661e016e55e5
rober-7 <rgonzaleez.37@gmail.com>
2025-06-18 05:12:03 -0300

    login en funcionamiento, deben agregarse mejoras visuales a login.html

---
+++ src/main/java/com/oo2/grupo20/entities/User.java
@@ -0,0 +1,51 @@
+package com.oo2.grupo20.entities;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter 
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 45)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "createdat")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updatedat")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+    private Set<UserRole> userRoles = new HashSet<>();
+
+    public User() {}
+}
